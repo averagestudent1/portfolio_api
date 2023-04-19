78233ecf811cbf02927c42bd7d7adc1eb5f43dd5
averagestudent1 <studentmail417@gmail.com>
2023-04-19 08:55:42 +0200

    use string-to-binary pkg

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log(5+8)
+//const convert=require('string-to-binary');
+import convert from "string-to-binary" //the same as previous line
+
+console.log(convert("Hello"));
+console.log(convert(2));
